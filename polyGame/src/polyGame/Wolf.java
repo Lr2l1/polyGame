5c5e35b2e1f1d2974698ab43ed31cc9734f7e97d
Lr2l <hoit5it@naver.com>
2024-04-01 18:30:00 +0900

    Add Wolf : attack

--- polyGame/src/polyGame/Wolf.java
+++ polyGame/src/polyGame/Wolf.java
@@ -1,7 +1,10 @@
 package polyGame;
 
-public class Wolf extends Monster {
+import java.util.Random;
 
+public class Wolf extends Monster {
+	private Random ran = new Random();
+	
 	Wolf() {
 		super("늑대", 100, 10);
 
@@ -10,7 +13,16 @@ public class Wolf extends Monster {
 	//전체공격
 	@Override
 	void attack(Unit enenmy) {
-		// TODO Auto-generated method stub
+		Human human = (Human) enenmy;
+		int ranPower = ran.nextInt(super.MAX_POWER);
+		setPower(ranPower);
+		human.setHp(human.getHp() - getPower());
+		System.out.printf("%s가 %s를 %d의 데미지로 공격\n", getName(), human.getName(), getPower());
+		
+		if (human.getHp() <= 0) {
+			human.setHp(0);
+			human.setIsDead();
+		}
 		
 	}
 
