161ac5e668db10950891446ce73d80b181c32095
Lr2l <hoit5it@naver.com>
2024-04-01 18:30:21 +0900

    Add Bat : attack

--- polyGame/src/polyGame/Bat.java
+++ polyGame/src/polyGame/Bat.java
@@ -1,15 +1,27 @@
 package polyGame;
 
-public class Bat extends Monster {
+import java.util.Random;
 
+public class Bat extends Monster {
+	private Random ran = new Random();
+	
 	Bat() {
 		super("박쥐", 100, 20);
-		
+
 	}
 
 	@Override
 	void attack(Unit enenmy) {
-		// 한명에게 침묵
+		Human human = (Human) enenmy;
+		int ranPower = ran.nextInt(super.MAX_POWER) + super.MAX_POWER;
+		setPower(ranPower);
+		human.setHp(human.getHp() - getPower());
+		System.out.printf("%s가 %s를 %d의 데미지로 공격\n", getName(), human.getName(), getPower());
+		human.silence();
+		if (human.getHp() <= 0) {
+			human.setHp(0);
+			human.setIsDead();
+		}
 
 	}
 }
