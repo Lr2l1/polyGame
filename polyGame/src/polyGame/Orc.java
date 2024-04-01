7ec9b5323991c0a6357b744b7450760e4230bb9f
Lr2l <hoit5it@naver.com>
2024-04-01 18:25:01 +0900

    Add Orc : attack

--- polyGame/src/polyGame/Orc.java
+++ polyGame/src/polyGame/Orc.java
@@ -3,15 +3,25 @@ package polyGame;
 import java.util.Random;
 
 public class Orc extends Monster {
+	private Random ran = new Random();
 
 	Orc() {
 		super("오크", 100, 20);
-		
+
 	}
 
-	//데미지 두배 + 기절
+	// 데미지 두배 + 기절
 	@Override
 	void attack(Unit enenmy) {
-		Random ran = new Random();
+		Human human = (Human) enenmy;
+		int ranPower = ran.nextInt(super.MAX_POWER) + super.MAX_POWER;
+		setPower(ranPower);
+		human.setHp(human.getHp() - getPower());
+		System.out.printf("%s가 %s를 %d의 데미지로 공격\n", getName(), human.getName(), getPower());
+		human.stun();
+		if (human.getHp() <= 0) {
+			human.setHp(0);
+			human.setIsDead();
+		}
 	}
 }
