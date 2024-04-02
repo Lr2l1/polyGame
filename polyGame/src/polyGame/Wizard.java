7c28236b20493c60e0f2ede0f06f879f6ed9bb40
Lr2l <hoit5it@naver.com>
2024-04-02 11:14:38 +0900

    Add Wizard : attack, skill and recover

--- polyGame/src/polyGame/Wizard.java
+++ polyGame/src/polyGame/Wizard.java
@@ -1,6 +1,9 @@
 package polyGame;
 
+import java.util.Random;
+
 public class Wizard extends Human {
+	private Random ran = new Random();
 
 	Wizard(String name, int hp, int power) {
 		super("법사", 800, 400, 70);
@@ -8,31 +11,45 @@ public class Wizard extends Human {
 
 	@Override
 	void attack(Unit enenmy) {
-		// TODO Auto-generated method stub
+		int ranPower = ran.nextInt(super.MAX_POWER);
 
+		setPower(ranPower);
+
+		System.out.printf("%s가 %s를 %d의 데미지로 공격\n", getName(), enenmy.getName(), getPower());
+		enenmy.setHp(getHp() - getPower());
+
+		if (enenmy.getHp() <= 0) {
+			enenmy.setHp(0);
+			enenmy.setIsDead();
+		}
 	}
 
 	@Override
-	void skill() {
-		// TODO Auto-generated method stub
+	void skill(Unit enenmy) {
+		setMp(getMp() - 40);
+		int ranPower = ran.nextInt(super.MAX_POWER);
+		setPower(ranPower);
+		System.out.printf("%s가 스킬을 사용하여 %d데미지\n", getName(), getPower());
 
+		if (enenmy.getHp() <= 0) {
+			enenmy.setHp(0);
+			enenmy.setIsDead();
+		}
 	}
 
 	@Override
 	public void stun() {
-		// TODO Auto-generated method stub
-
+		
 	}
 
 	@Override
 	public void silence() {
-		// TODO Auto-generated method stub
 
 	}
 
 	@Override
 	public void recover() {
-		// TODO Auto-generated method stub
+		setHp(getHp() + 50);
 
 	}
 }
