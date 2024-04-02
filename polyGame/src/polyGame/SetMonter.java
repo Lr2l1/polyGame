30da46128c81977c38066831ba77eb9e09bb579a
Lr2l1 <hoit5it@naver.com>
2024-04-02 15:02:34 +0900

    Add SetMonster : drawMonster

--- polyGame/src/polyGame/SetMonter.java
+++ polyGame/src/polyGame/SetMonter.java
@@ -9,12 +9,20 @@ public class SetMonter {
 	
 	private Random ran = new Random();
 	Map<Integer, Monster> monsters = new HashMap<Integer, Monster>();
+	String monster[] = { "Wolf", "Bat", "Orc" };
 	
 	private void drawMonster() {
-		
 		for (int i = 0 ; i < SIZE ; i ++) {
-			int dice = ran.nextInt(3)+1;
+			int dice = ran.nextInt(monster.length);
 			
+			try {
+				Class<?> clazz = Class.forName(monster[dice]);
+				Object obj = clazz.getDeclaredConstructor().newInstance();
+				Monster temp = (Monster) obj;
+				monsters.put(i,temp);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
 		}
 	}
 }
