5d88df008b67309d7ff57a8bf39196feeee6b4c5
Lr2l <hoit5it@naver.com>
2024-04-02 11:17:01 +0900

    Add Human : isSutn and isSilece method

--- polyGame/src/polyGame/Human.java
+++ polyGame/src/polyGame/Human.java
@@ -3,20 +3,38 @@ package polyGame;
 public abstract class Human extends Unit implements Stunable, Silenceable, Recoverable {
 	public final int MAX_MP;
 	private int mp;
+	private boolean isStun;
+	private boolean isSilence;
 
 	Human(String name, int hp, int mp, int power) {
 		super(name, hp, power);
 		this.MAX_MP = mp;
 	}
-	
+
 	public int getMp() {
 		return this.mp;
 	}
-	
+
 	public void setMp(int mp) {
 		this.mp = mp;
 	}
 
-	abstract void skill();
+	public boolean isStun() {
+		return this.isStun();
+	}
+
+	public void setIsStun() {
+		this.isStun = !isStun;
+	}
+
+	public boolean isSilence() {
+		return this.isSilence;
+	}
+
+	public void setIsSilence() {
+		this.isSilence = !isSilence;
+	}
+
+	abstract void skill(Unit unit);
 
 }
