ce5bd02a9af7a2861de14b4e1f65652e6bd23133
Lr2l1 <hoit5it@naver.com>
2024-04-04 18:21:23 +0900

    Modify : 아이템 판매기능

--- polyGame/src/polyGame/Inventory.java
+++ polyGame/src/polyGame/Inventory.java
@@ -8,13 +8,16 @@ public class Inventory {
 	Guild guild = new Guild();
 
 	public void printMenu() {
-		System.out.println("[1.착용] [2.판매] [0.뒤로가기]");
+		System.out.println("[1.착용] [2.해제] [3.판매] ");
+		System.out.println("[0.뒤로가기]");
 		int sel = Game.inputNumber("메뉴");
 		if (sel == 0)
 			return;
 		else if (sel == 1)
 			wearItem();
 		else if (sel == 2)
+			removeItem();
+		else if (sel == 3)
 			sellItem();
 	}
 
@@ -24,20 +27,42 @@ public class Inventory {
 			System.out.print("[" + (i + 1) + "번] ");
 			System.out.printf("[이름 : %s]", items.get(i).name);
 			System.out.printf("[능력 : %s]", items.get(i).power);
-			System.out.printf("[가격 : %s]", items.get(i).price);
 			System.out.println();
 		}
 	}
 
 	public void wearItem() {
+		guild.printPlayers();
+		int num = Game.inputNumber("플레이어번호");
+		guild.printWornItem(num);
+		printItem();
+		if (guild.players.get(num).getHelmet() == null) {
+			
+		} 
+//			guild.players.get(num).setHelmet();
+
+	}
+
+	public void removeItem() {
+		guild.printPlayers();
 		int sel = Game.inputNumber("플레이어번호");
 		guild.printWornItem(sel);
-		
+
 	}
 
 	public void sellItem() {
 		printItem();
+		int num = Game.inputNumber("상품번호")-1;
+		if (num<0 || num>=items.size() )
+			return;
+		
+		Human.money += items.get(num).price/2;
+		items.remove(num);
+		
+		
+		
 	}
+	
 
 	public void addItem(Item item) {
 		items.add(item);
