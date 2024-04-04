688e87695fe5c2be58769ee0f1c611642f8c173e
Lr2l1 <hoit5it@naver.com>
2024-04-04 15:50:58 +0900

    Modify : 생성자 생성

--- polyGame/src/polyGame/Main.java
+++ polyGame/src/polyGame/Main.java
@@ -2,8 +2,14 @@ package polyGame;
 
 public class Main {
 	public static void main(String[] args) {
-		Game game = new Game();
-		game.run();
+		boolean isRun = true;
+		Game.instance.init();
+		while (true) {
+			isRun = Game.instance.changeStage();
+			if (isRun == false) {
+				break;
+			}
+		}
+		System.out.println("게임 종료");
 	}
-
 }
