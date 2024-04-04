793f60a9f544d3d006c2a9f2ffc367c2d2297818
Lr2l1 <hoit5it@naver.com>
2024-04-04 15:28:01 +0900

    Modify : public제어자 추가

--- polyGame/src/polyGame/Item.java
+++ polyGame/src/polyGame/Item.java
@@ -1,13 +1,13 @@
 package polyGame;
 
 public class Item {
-	static final int WEAPON = 1;
+	static final int HELMET = 1;
 	static final int ARMOR = 2;
 	static final int RING = 3;
-	int kind;
-	String name;
-	int power;
-	int price;
+	public int kind;
+	public String name;
+	public int power;
+	public int price;
 
 	public void setItem(int kind, String name, int power, int price) {
 		this.kind = kind;
