e6f1023e88c09048321daaf5298f165e06d36b2f
Lr2l1 <hoit5it@naver.com>
2024-04-04 12:33:02 +0900

    Modify : 인벤토리와 연결하여 구매시 아이템 인벤토리에 추가

--- polyGame/src/polyGame/Shop.java
+++ polyGame/src/polyGame/Shop.java
@@ -4,6 +4,8 @@ import java.util.ArrayList;
 
 public class Shop {
 	ArrayList<Item> items = new ArrayList<Item>();
+	Game game = new Game();
+	Inventory inventory = new Inventory();
 
 	public Shop() {
 		Item item = new Item();
@@ -59,6 +61,62 @@ public class Shop {
 		item.name = "다이아반지";
 		item.power = 35;
 		item.price = 15000;
-
 	}
+
+	private void shopMenu() {
+		System.out.println("================");
+		System.out.println("[1]투구 [2]갑옷 [3]반지");
+		int kind = game.inputNumber("메뉴");
+
+		while (true) {
+			if (kind == Item.HELMET) {
+				System.out.println("=========== [헬멧 ============");
+			} else if (kind == Item.ARMOR) {
+				System.out.println("========== [방어구] ===========");
+			} else if (kind == Item.RING) {
+				System.out.println("=========== [반지] ============");
+			}
+			printItem(kind);
+			System.out.println("------------------------------------");
+			System.out.println("[골드 : ]");
+			System.out.println("구입할 아이템 번호를 입력하세요 [0.뒤로가기]");
+			int selNum = game.inputNumber("메뉴");
+			if (selNum == 0)
+				break;
+			int count = 0;
+			for (int i = 0; i < items.size(); i++) {
+
+				if (items.get(i).kind == kind) {
+					count += 1;
+
+					if (count == selNum) {
+						inventory.addItem(items.get(i));
+						Human.money -= items.get(i).price;
+						System.out.println("[" + items.get(i).name + "] 을 구입했습니다.");
+						try {
+							Thread.sleep(1000);
+						} catch (InterruptedException e) {
+							e.printStackTrace();
+						}
+						break;
+					}
+				}
+			}
+		}
+	}
+
+	private void printItem(int kind) {
+		int count = 0;
+		for (int i = 0; i < items.size(); i++) {
+			if (items.get(i).kind != kind)
+				continue;
+			System.out.print("[" + (count + 1) + "번] ");
+			System.out.print("[이름 : " + items.get(i).name + "] ");
+			System.out.print("[능력 : " + items.get(i).power + "] ");
+			System.out.print("[가격 : " + items.get(i).price + "] ");
+			System.out.println();
+			count++;
+		}
+	}
+
 }
