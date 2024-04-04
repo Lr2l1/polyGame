204f4444adb7b2c6504db83e19c5550f0efe7794
Lr2l1 <hoit5it@naver.com>
2024-04-04 17:14:57 +0900

    Modify : 종료하기를 뒤로가기 기능으로 교체

--- polyGame/src/polyGame/StageSetting.java
+++ polyGame/src/polyGame/StageSetting.java
@@ -7,10 +7,11 @@ public class StageSetting extends Stage {
 
 	@Override
 	public boolean update() {
-		while (true) {
+		boolean isRun = true;
+		while (isRun) {
 			System.out.println("=============== [메인메뉴] ================");
 			System.out.println("[1.길드관리]\t\t[2.상점]\t\t[3.인벤토리]");
-			System.out.println("[4.저장]\t\t[5.로드]\t\t[0.종료]");
+			System.out.println("[4.저장]\t\t[5.로드]\t\t[0.뒤로가기]");
 			int sel = Game.inputNumber("메뉴");
 			if (sel == 1) {
 				guild.printMenu();
@@ -23,7 +24,8 @@ public class StageSetting extends Stage {
 			} else if (sel == 5) {
 
 			} else if (sel == 0) {
-				break;
+				Game.nextStage = "LOBBY";
+				isRun = false;
 			}
 		}
 		return false;
