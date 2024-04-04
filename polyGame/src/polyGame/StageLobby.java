ee544e7d3e961ed4021df6601a1c490169733694
Lr2l1 <hoit5it@naver.com>
2024-04-04 17:13:31 +0900

    Modify : 로비에서 이동 추가

--- polyGame/src/polyGame/StageLobby.java
+++ polyGame/src/polyGame/StageLobby.java
@@ -3,26 +3,20 @@ package polyGame;
 public class StageLobby extends Stage {
 	@Override
 	public boolean update() {
-
-		while (true) {
-			System.out.println("=====[LOBBY]=====");
-			System.out.println("[1. 전투] [2. 설정] [0. 종료]");
-			int sel = Game.inputNumber("메뉴");
-			if (sel == 1) {
-
-			} else if (sel == 2) {
-
-			}
-			if (sel == 0)
-				break;
-		}
+		System.out.println("=====[LOBBY]=====");
+		System.out.println("[1. 전투] [2. 설정] [0. 종료]");
+		int sel = Game.inputNumber("메뉴");
+		if (sel == 1)
+			Game.nextStage = "BATTLE";
+		else if (sel == 2)
+			Game.nextStage = "SETTING";
+		else if (sel == 0)
+			Game.nextStage = "";
 
 		return false;
 	}
 
 	@Override
 	public void init() {
-
 	}
-
 }
