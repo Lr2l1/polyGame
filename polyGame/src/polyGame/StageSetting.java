9b9de27b4119a1c801ed0b72b540c00e1856fc26
Lr2l1 <hoit5it@naver.com>
2024-04-04 15:20:21 +0900

    Add : 스테이지 메뉴추가

--- polyGame/src/polyGame/StageSetting.java
+++ polyGame/src/polyGame/StageSetting.java
@@ -1,5 +1,38 @@
 package polyGame;
 
-public class StageSetting {
+public class StageSetting extends Stage {
+	Game game = new Game();
+	Shop shop = new Shop();
+	Guild guild = new Guild();
+	Inventory inventory = new Inventory();
+
+	@Override
+	public boolean update() {
+		while (true) {
+			System.out.println("=============== [메인메뉴] ================");
+			System.out.println("[1.길드관리]\t\t[2.상점]\t\t[3.인벤토리]");
+			System.out.println("[4.저장]\t\t[5.로드]\t\t[0.종료]");
+			int sel = game.inputNumber("메뉴");
+			if (sel == 1) {
+				guild.printMenu();
+			} else if (sel == 2) {
+				shop.printMenu();
+			} else if (sel == 3) {
+				inventory.printItem();
+			} else if (sel == 4) {
+
+			} else if (sel == 5) {
+
+			} else if (sel == 0) {
+				break;
+			}
+		}
+		return false;
+	}
+
+	@Override
+	public void init() {
+
+	}
 
 }
