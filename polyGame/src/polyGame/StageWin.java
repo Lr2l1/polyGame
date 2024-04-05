45ddb3aa18428d757c5d390e36517d4509172082
Lr2l1 <hoit5it@naver.com>
2024-04-05 12:39:22 +0900

    Add : 승리시 레벨업 추가

--- polyGame/src/polyGame/StageWin.java
+++ polyGame/src/polyGame/StageWin.java
@@ -1,17 +1,29 @@
 package polyGame;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class StageWin extends Stage {
+	Map<Integer, Human> players = new HashMap<>();
+	Guild guild = new Guild();
 
 	@Override
 	public boolean update() {
 		System.out.println("승리~");
+		
+		for (int i = 0; i < Guild.players.size(); i++) {
+			Guild.players.get(i).levelUp();
+		}
+		for (int i = 0; i < Guild.players.size(); i++)
+			System.out.println(Guild.players.get(i));
+		
 		Game.nextStage = "LOBBY";
 		return false;
 	}
 
 	@Override
 	public void init() {
-
+	
 	}
 
 }
