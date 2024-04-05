8186a157dcdae0ec099d2bbf12c7b763efc0511f
Lr2l1 <hoit5it@naver.com>
2024-04-05 12:49:11 +0900

    Add : 승리시 아이템 드랍추가

--- polyGame/src/polyGame/StageWin.java
+++ polyGame/src/polyGame/StageWin.java
@@ -1,29 +1,36 @@
 package polyGame;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.Random;
 
 public class StageWin extends Stage {
-	Map<Integer, Human> players = new HashMap<>();
-	Guild guild = new Guild();
+	Random ran = new Random();
 
 	@Override
 	public boolean update() {
 		System.out.println("승리~");
-		
-		for (int i = 0; i < Guild.players.size(); i++) {
-			Guild.players.get(i).levelUp();
-		}
-		for (int i = 0; i < Guild.players.size(); i++)
-			System.out.println(Guild.players.get(i));
-		
+		levelUp();
+		dropItem();
+
 		Game.nextStage = "LOBBY";
 		return false;
 	}
 
 	@Override
 	public void init() {
-	
+
 	}
 
+	public void levelUp() {
+		for (int i = 0; i < Guild.players.size(); i++) {
+			Guild.players.get(i).levelUp();
+		}
+		for (int i = 0; i < Guild.players.size(); i++)
+			System.out.println(Guild.players.get(i));
+	}
+
+	public void dropItem() {
+		int dice = ran.nextInt(Shop.itemList.size());
+
+		Inventory.addItem(Shop.itemList.get(dice));
+	}
 }
