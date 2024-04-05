4f23fc7fa30ab6858e3379adf99b555ae21d0963
Lr2l1 <hoit5it@naver.com>
2024-04-05 14:09:25 +0900

    Modify : 지정된 숫자 이외에 입력시 재입력

--- polyGame/src/polyGame/StageLobby.java
+++ polyGame/src/polyGame/StageLobby.java
@@ -5,14 +5,19 @@ public class StageLobby extends Stage {
 	public boolean update() {
 		System.out.println("=====[LOBBY]=====");
 		System.out.println("[1. 전투] [2. 설정] [0. 종료]");
-		int sel = Game.inputNumber("메뉴");
-		if (sel == 1)
-			Game.nextStage = "BATTLE";
-		else if (sel == 2)
-			Game.nextStage = "SETTING";
-		else if (sel == 0)
-			Game.nextStage = "";
-
+		while (true) {
+			int sel = Game.inputNumber("메뉴");
+			if (sel == 1) {
+				Game.nextStage = "BATTLE";
+				break;
+			} else if (sel == 2) {
+				Game.nextStage = "SETTING";
+				break;
+			} else if (sel == 0) {
+				Game.nextStage = "";
+				break;
+			}
+		}
 		return false;
 	}
 
