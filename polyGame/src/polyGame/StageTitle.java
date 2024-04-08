112f51693de10232c9ae1b6a951670f92746b0db
Lr2l1 <hoit5it@naver.com>
2024-04-08 16:46:42 +0900

    Modify : 타이틀 이미지 수정

--- polyGame/src/polyGame/StageTitle.java
+++ polyGame/src/polyGame/StageTitle.java
@@ -4,11 +4,21 @@ public class StageTitle extends Stage {
 
 	@Override
 	public boolean update() {
-		System.out.println("├┬┴┬┴┬┴┤TETX  RPG├┬┴┬┴┬┴┬┤");
-		System.out.println("├   화면에 숫자를 입력하세요  ┤");
-		System.out.println("├┬┴┬┴┬┴┬┴┬┴┬┴┬┴┬┴┬┴┬┴┬┴┬┴┤");
+		System.out.println("                       -|             |-");
+		System.out.println("   -|                  [-_-_-_-_-_-_-_-]                  |-");
+		System.out.println("   [-_-_-_-_-]          |             |          [-_-_-_-_-]");
+		System.out.println("    | o   o |           [  0   0   0  ]           | o   o |");
+		System.out.println("     |     |    -|       |           |       |-    |     |");
+		System.out.println("     |     |_-___-___-___-|         |-___-___-___-_|     |");
+		System.out.println("     |  o  ]              [    0    ]              [  o  |");
+		System.out.println("     |     ]   o   o   o  [ _______ ]  o   o   o   [     | ----");
+		System.out.println("---- |     ]              [ ||||||| ]              [     |");
+		System.out.println("     |     ]              [ ||||||| ]              [     |");
+		System.out.println(" _-_-|_____]--------------[_|||||||_]--------------[_____|-_-_");
+		System.out.println("( (__________------------_____________-------------_________)");
+		System.out.println("\t├┬┴┬┴┬┴┬┴┬┴┬┴┬┴┬┴┤TETX  RPG├┬┴┬┴┬┴┬┴┬┴┬┴┬┴┬┴┬┴┬┤");
+		System.out.println("화면에 숫자를 입력해주세요.");
 		Game.inputNumber("");
-		
 		Game.nextStage = "LOBBY";
 		return false;
 	}
