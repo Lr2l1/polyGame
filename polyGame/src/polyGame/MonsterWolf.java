1590fb67f96f09314231779a45cc3177a7150fb2
Lr2l1 <hoit5it@naver.com>
2024-04-09 10:18:35 +0900

    Modify : 단일공격에서 전체공격으로 수정

--- polyGame/src/polyGame/MonsterWolf.java
+++ polyGame/src/polyGame/MonsterWolf.java
@@ -13,15 +13,17 @@ public class MonsterWolf extends Monster {
 	// 전체공격
 	@Override
 	void attack(Unit enenmy) {
-		Human human = (Human) enenmy;
-		int ranPower = ran.nextInt(super.MAX_POWER);
-		setPower(ranPower);
-		human.setHp(human.getHp() - getPower());
-		System.out.printf("%s가 %s를 %d의 데미지로 공격\n", getName(), human.getName(), getPower());
+		for (int i = 0; i < Guild.players.size(); i++) {
+			Human human = Guild.players.get(i);
+			int ranPower = ran.nextInt(super.MAX_POWER);
+			setPower(ranPower);
+			human.setHp(human.getHp() - getPower());
+			System.out.printf("%s가 %s를 %d의 데미지로 공격\n", getName(), human.getName(), getPower());
 
-		if (human.getHp() <= 0) {
-			human.setHp(0);
-			human.setIsDead(true);
+			if (human.getHp() <= 0) {
+				human.setHp(0);
+				human.setIsDead(true);
+			}
 		}
 	}
 }
